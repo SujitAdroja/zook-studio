9bf1da88892d5c15761c7303ce9ea3660890084d
Sujit Adroja <sujitadroja07@gmail.com>
2025-09-19 00:12:17 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,31 @@
+const projects = [
+  {
+    id: 1,
+    image: "/homepage/hero-image.jpg",
+    title: "Best Residential Interior Apartment Ahembdabad",
+    projectTitle: "Serenity Lake Apartment",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus voluptatum fugiat eos perferendis neque dolore.",
+    year: 2024,
+  },
+  {
+    id: 2,
+    image: "/homepage/hero_image.jpg",
+    title: "Best Residential Interior Apartment Romania",
+    projectTitle: "Serenity Lake Apartment",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus voluptatum fugiat eos perferendis neque dolore.",
+    year: 2025,
+  },
+  {
+    id: 3,
+    image: "/homepage/image-5.jpg",
+    title: "Best Residential Interior Apartment Europe",
+    projectTitle: "Serenity Lake Apartment",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus voluptatum fugiat eos perferendis neque dolore.",
+    year: 2025,
+  },
+];
+
+export { projects };
